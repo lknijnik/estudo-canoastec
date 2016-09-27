7e245cb65dc29d9afbf97b9649244fa4ff07c1cf
Leonel Knijnik <leonelkni@gmail.com>
2016-09-27 11:26:45 -0300

    Criação das controllers e da migration para a tabela de produtos

---
+++ database/migrations/2016_09_27_131704_create_table_products.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+
+            $table->engine = 'InnoDB';
+
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->text('short_description')->nullable();
+            $table->string('url', 255)->nullable();
+            $table->string('sku', 64);
+            $table->string('grouping_sku', 64)->nullable();
+            $table->string('isbn', 17)->nullable();
+            $table->integer('stock')->nullable();
+            $table->integer('minimum_stock')->nullable();
+            $table->decimal('price', 15,2);
+            $table->decimal('discount_price', 15,2)->nullable();
+            $table->tinyInteger('discount_percentage')->nullable();
+            $table->decimal('weight', 15,2);
+            $table->decimal('width', 15,2);
+            $table->decimal('length', 15,2);
+            $table->decimal('height', 15,2);
+            $table->text('description');
+            $table->boolean('status');
+            $table->smallInteger('views')->nullable();
+            $table->date('available_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
